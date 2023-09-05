a19c740975dc502cc1dbaad5614ee73d9dfe7be2
NeneNafe <nenechiwaya@gmail.com>
2023-09-05 13:36:00 -0700

    Uppercases

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            upper = chr(ord(char) - ord('a') + ord('A'))
+            result += upper
+        else:
+            result += char
+    print(result)
