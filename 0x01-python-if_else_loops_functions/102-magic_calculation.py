66d4b6447f2de72c62d8fc2944300b566fb55925
NeneNafe <nenechiwaya@gmail.com>
2023-09-06 02:17:27 -0700

    ByteCode -> Python #2

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,4 +3,4 @@ def magic_calculation(a, b, c):
     if a < b:
         return c ** 2
     else:
-        raise Exception("a cannot be greater than or ewual to b")
+        raise Exception("a cannot be greater than or equal to b")
