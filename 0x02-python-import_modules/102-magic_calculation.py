28e0c25cf1fcaa9074f48f2bc22db592dc17b4e2
NeneNafe <nenechiwaya@gmail.com>
2023-09-07 15:33:14 -0700

    function def magic_calculation(a, b): that does exactly

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python3
-from magic_calculation_102 import add, sub
+#!/usr/bin/pythons3
 
 def magic_calculation(a, b):
+    """Matches bytecode that is provided"""
+    from magic_calculation_102 import, sub
 
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-     else:
-         return (sub(a, b))
+
+    else:
+        return(sub(a, b))
