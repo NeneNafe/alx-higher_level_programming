33f7315cb60ee7f2e5d00433f93b49edc4766edb
NeneNafe <nenechiwaya@gmail.com>
2023-09-07 11:44:55 -0700

    imports functions from the file calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,6 +5,6 @@ a = 10
 b = 5
 
 print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
