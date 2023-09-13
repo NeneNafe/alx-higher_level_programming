8547a2967712e6859117ac6cd0038bf47224d8e2
NeneNafe <nenechiwaya@gmail.com>
2023-09-13 12:47:08 -0700

    bug fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,15 +4,7 @@ def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
 
-    roman_dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D':500,
-            'M': 1000
-            }
+    roman_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
     prev, sum = 0, 0
 
