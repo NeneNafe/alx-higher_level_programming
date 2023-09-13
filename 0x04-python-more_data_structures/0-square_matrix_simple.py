53554b3f07a63e5224be9ad6380e91dca49811f1
NeneNafe <nenechiwaya@gmail.com>
2023-09-13 04:09:02 -0700

    a readme file and a function that prints the squares of intergers found in a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    result_mat = []
+    
+    for row in matrix:
+        result = []
+
+        for element in row:
+            result.append(element ** 2)
+        result_mat.append(result)
+    return result_mat
