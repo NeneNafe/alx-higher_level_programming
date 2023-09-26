1ef752fb6e3ba7f1995cbf701a7b3c29d7215423
NeneNafe <nenechiwaya@gmail.com>
2023-09-26 12:18:44 -0700

    edited

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+# 0-square.py by Alina_Nene
+"""An empty class Square"""
+
 class Square:
+
     """This is a python3 program that write an empty
     class Square that define a square. No modules were imported"""
+
     pass
+    """initialiazes the square class, by taking size and an arg"""
