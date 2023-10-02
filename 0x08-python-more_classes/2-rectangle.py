0e3af517a0341529353df873574f4ee8146304cf
NeneNafe <nenechiwaya@gmail.com>
2023-10-02 12:27:58 -0700

    2-rectangle.py)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -17,11 +17,13 @@ class Rectangle:
         return self.__width * self.__height
     def perimeter(self):
         """This part returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
 
     @property
     def width(self):
-        """Retrieves the width"""
+        """Retrieves the width of the rectangle"""
         return self.__width
 
     @width.setter
