c1f38f9474da296ba13ce02dcfde54c12595a6d2
NeneNafe <nenechiwaya@gmail.com>
2023-10-10 05:57:05 -0700

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ def read_file(filename=""):
     """A function that reads a text file (UTF8)
     and prints it to stdout"""
 
-    with open (filename, 'r', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end="")
