33fbc91b54328e93dd779a331b1f23a87316cc49
NeneNafe <nenechiwaya@gmail.com>
2023-10-10 11:08:08 -0700

    a bfunction that creates an onject from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a function"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    a function that creates an Object from a “JSON file”
+    """
+
+    with open(filename, 'r+', encoding='utf-8') as file:
+        return json.load(file)
