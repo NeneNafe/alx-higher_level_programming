4d15649348e36a959b54529a3c554af8b8ba9f2e
NeneNafe <nenechiwaya@gmail.com>
2023-10-10 06:21:25 -0700

    2. Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,5 +9,5 @@ def append_write(filename="", text=""):
     """
 
     with open(filename, 'a', encoding='utf-8') as file:
-            Numchar_append = file.write(text)
-            return Numchar_append
+        Numchar_append = file.write(text)
+        return Numchar_append
