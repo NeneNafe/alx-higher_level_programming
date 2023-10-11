a0decf210062614c2c33e943155aa44b3f52a3c2
NeneNafe <nenechiwaya@gmail.com>
2023-10-11 02:39:32 -0700

    changes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -23,25 +23,16 @@ if __name__ == "__main__":
 
     try:
         for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
             line = line.split()
 
             try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
+                status_code = int(line[-2])
+                if status_code in valid_codes:
+                    status_codes[status_codes] = status_codes.get(status_code, 0) + 1
+                size += int(line[-1])
+            except (ValueError, IndexError):
                 pass
-        print_stats(size, status_codes)
-
 
     except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+        pass
+    print_stats(size, status_codes)
