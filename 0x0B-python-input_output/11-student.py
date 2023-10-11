33f89d127f9cec6c889a5298c41cfd50843d7e15
NeneNafe <nenechiwaya@gmail.com>
2023-10-11 01:01:40 -0700

    a class Student that defines a student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class Student:
+    """
+    This is a class that defines a student by:
+    first name, last name, and their age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        initialises three public instances of class student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student"""
+
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
