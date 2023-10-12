869d8a9d966df3a1d9f20405a2226f6ff29531e1
NeneNafe <nenechiwaya@gmail.com>
2023-10-12 14:42:16 -0700

    an empty file __init__.py and a file named models/base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a super class Base"""
+
+class Base:
+
+    __nb_objects = 0
+    """This is a private class attribute"""
+
+    def __init__(self, id=None):
+        """
+        Initialisez the class Base by taking arguments like id
+        which is initialised to Noe
+        """
+
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
