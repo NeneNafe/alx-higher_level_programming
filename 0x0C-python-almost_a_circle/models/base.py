4f010b2b4cdb87f0fb39466dd4ec2c41690acf19
NeneNafe <nenechiwaya@gmail.com>
2023-10-16 05:43:10 -0700

    returns  the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines a super class Base"""
 
+import json
+
 
 class Base:
 
@@ -18,3 +20,11 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
