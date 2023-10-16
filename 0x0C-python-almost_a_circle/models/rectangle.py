5277e92537b9800f4bb7a7d2d7ac97b794fc2bb6
NeneNafe <nenechiwaya@gmail.com>
2023-10-16 03:54:19 -0700

    changess

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,17 +15,16 @@ class Rectangle(Base):
         attributes, that each has it own public getter & setter
         """
 
-        self.id = id
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
         super().__init__(id)
 
     def area(self):
         """returns the area of the rectangle instance"""
-        return self.__width * self.__height
+        return self.width * self.height
 
         @property
         def width(self):
@@ -73,10 +72,10 @@ class Rectangle(Base):
 
     def display(self):
         """displays the rectangls with character #"""
-        print("\n" * self.__y, end="")
-        for i in range(self.__height):
-            print(" " * self.__x, end="")
-            print("#" * self.__width, end="")
+        print("\n" * self.y, end="")
+        for i in range(self.height):
+            print(" " * self.x, end="")
+            print("#" * self.width, end="")
             print()
 
     def __str__(self):
@@ -90,22 +89,22 @@ class Rectangle(Base):
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
-                self.__width = args[1]
+                self.width = args[1]
             if len(args) >= 3:
-                self.__height = args[2]
+                self.height = args[2]
             if len(args) >= 4:
-                self.__x = args[3]
+                self.x = args[3]
             if len(args) >= 5:
-                self.__y = args[4]
+                self.y = args[4]
 
         if kwargs:
             if 'id' in kwargs:
                 self.id = kwargs['id']
             if 'width' in kwargs:
-                self.__width = kwargs['width']
+                self.width = kwargs['width']
             if 'height' in kwargs:
-                self.__height = kwargs['height']
+                self.height = kwargs['height']
             if 'x' in kwargs:
-                self.__x = kwargs['x']
+                self.x = kwargs['x']
             if 'y' in kwargs:
-                self.__y = kwargs['y']
+                self.y = kwargs['y']
