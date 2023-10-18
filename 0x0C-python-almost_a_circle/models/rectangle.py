55eb114b3b6f5448fbd22ab677abebb444f08a1d
NeneNafe <nenechiwaya@gmail.com>
2023-10-18 02:19:55 -0700

    corrections

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,61 +39,61 @@ class Rectangle(Base):
 
         super().__init__(id)
 
-        @property
-        def width(self):
-            """retrieves the attribute instance"""
-            return self.__width
+    @property
+    def width(self):
+        """retrieves the attribute instance"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """a setter method for width"""
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """a setter method for width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """a getter method"""
-            return self.__height
+    @property
+    def height(self):
+        """a getter method"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """a setter method for height"""
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """a setter method for height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """retrieves the attribute instance"""
-            return self.__x
+    @property
+    def x(self):
+        """retrieves the attribute instance"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """sets value to attribute"""
-            if type(value) is not int:
-                raise TypeError("x must be an integer")
-            if value < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """sets value to attribute"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """retrieves the attribute instance"""
-            return self.__y
+    @property
+    def y(self):
+        """retrieves the attribute instance"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """sets value to attribute"""
-            if type(value) is not int:
-                raise TypeError("y must be an integer")
-            if value < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """sets value to attribute"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
     def area(self):
         """returns the area of the rectangle instance"""
