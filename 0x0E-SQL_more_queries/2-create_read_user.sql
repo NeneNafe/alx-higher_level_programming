7cc37357ca12f702ed8ee253969a4266f9c9e04c
NeneNafe <nenechiwaya@gmail.com>
2023-11-15 23:23:54 +0200

    Corrections

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@
 CREATE database IF NOT EXITS hbtn_0d_2;
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGE
+FLUSH PRIVILEGEs;
