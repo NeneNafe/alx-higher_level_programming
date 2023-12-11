6bff6c5583e7b31297676b9a50621b73f827ba20
NeneNafe <nenechiwaya@gmail.com>
2023-12-11 23:17:56 +0200

    yo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
-const args = process.argv.slice(2);
+#!/usr/bin/node
 
-if (args.length === 0) { console.log('No argument'); } else if (args.length === 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
+if (process.argv.length === 2) { console.log('No argument'); } else if (process.argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
