e9e01b89cf9649fd38556a6532f083b6e9285fc7
NeneNafe <nenechiwaya@gmail.com>
2023-12-12 23:00:58 +0200

    Task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * A rectangle class
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
