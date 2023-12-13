04679b25f406ad024b5bf73211ace68690b282d4
NeneNafe <nenechiwaya@gmail.com>
2023-12-13 12:53:59 +0200

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+/**
+ * Defines a class functionality
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
+      console.log(myVar);
+    }
+  }
+
+  rotate () {
+    if (this.width && this.height) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
+
+  double () {
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+module.exports = Rectangle;
