bb6c7853d79a374e55ca56a8228331efb2175635
NeneNafe <nenechiwaya@gmail.com>
2023-12-13 13:46:27 +0200

    Task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node 
+/**
+ * returns args already printes and new arg value
+ */
+
+let counter = 0;
+
+exports.logMe = function count (item) {
+    console.log('${counter}: ${item)');
+    count += 1;
+};
