e12676c0025f6c37d3f4e0229a34cf9ec6ec56d3
NeneNafe <nenechiwaya@gmail.com>
2023-12-17 20:01:24 +0200

    prints the first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     first_state = session.query(State).first()
     if first_state is None:
         print("Nothing")
