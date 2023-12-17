7309e873e9f6deb2027f8e87e73ba77e2c28557b
NeneNafe <nenechiwaya@gmail.com>
2023-12-17 23:09:56 +0200

    Corrections

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """a script that prints the State object with the name passed
 as argument from the database hbtn_0e_6_usa"""
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    """Gets the state fro mthe database"""
     database = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
