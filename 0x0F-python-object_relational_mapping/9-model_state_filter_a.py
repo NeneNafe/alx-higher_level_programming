8c9e411694de362f8dcc74b68441402078d48941
NeneNafe <nenechiwaya@gmail.com>
2023-12-17 21:04:15 +0200

    Corrections

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,6 +14,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
 for states in session.query(State).filter(State.name.like('%a%')):
     print(states.id, states.name, sep=": ")
