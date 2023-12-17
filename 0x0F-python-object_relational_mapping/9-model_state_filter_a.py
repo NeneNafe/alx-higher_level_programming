5e72edc37102001a7cbbc4a67a5c82ee1ad41f83
NeneNafe <nenechiwaya@gmail.com>
2023-12-17 21:01:57 +0200

    prints all states that have an a in their names

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a script that lists all State objects from the database hbtn_0e_6_usa"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """checks if the correct number of args is provided"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+for states in session.query(State).filter(State.name.like('%a%')):
+    print(states.id, states.name, sep=": ")
