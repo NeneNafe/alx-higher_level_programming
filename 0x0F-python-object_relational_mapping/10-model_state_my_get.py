bf4e57bf3be7d1779b945dfd6db2d0c5a99a1c03
NeneNafe <nenechiwaya@gmail.com>
2023-12-17 23:18:31 +0200

    Corrections

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,19 +5,19 @@ as argument from the database hbtn_0e_6_usa"""
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
     """Gets the state fro mthe database"""
-    database = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    engine = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create_engine(database)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == argv[4]).first()
-    if state is not None:
-        print('{0}'.format(state.id))
+    for state in session.query(State):
+        if (state.name == argv[4]):
+            print(state.id)
+            break
     else:
         print("Not Found")
