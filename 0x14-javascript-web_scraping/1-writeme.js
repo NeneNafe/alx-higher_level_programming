bcc77650ee0959123fc323062ab25780194d2f1a
NeneNafe <nenechiwaya@gmail.com>
2024-01-30 13:16:35 +0200

    Corrections

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filename = process.argv[2];
-const content = process.argv[3];
 
 fs.readFile(filename, 'utf-8', (error, content) => {
   if (error) {
