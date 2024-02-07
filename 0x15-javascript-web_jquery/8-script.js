869e71d1097065909bba9e7a9c19e14b6a2c7587
NeneNafe <nenechiwaya@gmail.com>
2024-02-07 21:37:03 +0200

    a JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  const films = data.results;
+  for (const film of films) {
+    $('UL#list_movies').append(`<li>${film.title}</li>`);
+  }
+});
